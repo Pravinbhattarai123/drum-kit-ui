5e5a277e7ce277eaa4844ed7770766906b692754
Pravinbhattarai123 <126901083+Pravinbhattarai123@users.noreply.github.com>
2024-09-01 22:20:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+var numofdrumnuttons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numofdrumnuttons; i++) {
+
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var bottominnerhtml = this.innerHTML;
+
+        makeSound(bottominnerhtml);
+        buttonanimation(bottominnerhtml);
+    });
+    
+    
+}
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonanimation(event.key)
+});
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default: console.log(bottominnerhtml)
+            break;
+    }
+};
+function buttonanimation(currentkey){
+ var activebutton=document.querySelector("."+currentkey);
+ activebutton.classList.add("pressed");
+
+ setTimeout(function(){
+    activebutton.classList.remove("pressed")
+ },100);
+
+}
+// var audio=new Audio("sounds/tom-1.mp3");
+// audio.play();
+
+
+// document.querySelector("button").addEventListener("click",function (){ (unanimous function called )
+//     alert("i got clicked")
+// });
+
